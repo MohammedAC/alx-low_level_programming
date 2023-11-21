18516b25c646d48bde26cb34be26f9d13778a309
Mohammed <med03ac@gmail.com>
2023-11-20 17:58:21 -0800

    test

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,23 +1,16 @@
-#include "main.h"
-/**
-  * _pow_recursion - function name
-  * @x: parameter 1
-  * @y: parameter 2
-  * Return: value of a given number passed to the function
-  */
-
-
 int _pow_recursion(int x, int y)
 {
+	int even;
 
-if (y < 0)
-        return (-1);
-
-else if (y == 0)
-        return (1);
-
-else
-        return (x * _pow_recursion(x, (y - 1)));
-
-return (0);
+	if (y < 0)		/*if y is a negative number, return -1*/
+		return (-1);
+	else if (y == 0)	/*x to the power of 0 is 1*/
+		return (1);
+	else if (y % 2 == 0)	/*if y is an even number*/
+	{
+		even = _pow_recursion(x, y / 2);
+		return (even * even);
+	}
+	else			/*if y is odd*/
+		return (x * _pow_recursion(x, y - 1));
 }
